55e7922a34cd09f871353b4421481c5bf42d5107
Anton Beliaev <anton.beliaev@student.torrens.edu.au>
2023-12-02 14:44:53 +1030

    Don't start and close session for each query

--- repositories.py
+++ repositories.py
@@ -6,43 +6,34 @@ from entities import Task
 class TaskRepository:
     def __init__(self, engine: Engine):
         self.engine = engine
+        self.session = Session(self.engine)
 
     def get_by_id(self, task_id: int) -> Task | None:
-        session = Session(self.engine)
         stmt = select(Task).where(Task.id == task_id)
-        entity = session.scalars(stmt).one_or_none()
-        session.close()
+        entity = self.session.scalars(stmt).one_or_none()
 
         return entity
 
     def get_all(self) -> list[Task]:
-        session = Session(self.engine)
         stmt = select(Task)
-        entities = session.scalars(stmt).all()
-        session.close()
+        entities = self.session.scalars(stmt).all()
 
         return entities
 
     def insert(self, entity: Task) -> Task:
-        with Session(self.engine) as session:
-            session.add(entity)
-            session.commit()
-            session.close()
+        self.session.add(entity)
+        self.session.commit()
 
         return entity
 
     def update(self, entity: Task) -> Task:
-        with Session(self.engine) as session:
-            session.add(entity)
-            session.commit()
-            session.close()
+        self.session.add(entity)
+        self.session.commit()
 
         return entity
 
     def delete(self, entity: Task) -> bool:
-        with Session(self.engine) as session:
-            session.delete(entity)
-            session.commit()
-            session.close()
+        self.session.delete(entity)
+        self.session.commit()
 
         return True
